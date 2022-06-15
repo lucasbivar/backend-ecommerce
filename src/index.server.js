edb615f84981d472a1722dc2f66d81e206a6753e
Lucas <lucasbivarfonseca@gmail.com>
2022-06-15 13:09:09 -0300

    creating route to signup admin/user

---
+++ src/index.server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const env = require("dotenv")
+const app = express();
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+//routes
+const userRoutes = require("./routes/user");
+
+
+// environment variable or you can say constants
+env.config();
+
+//mongodb connection
+//mongodb+srv://root:adminpassword@cluster0.gqtpff3.mongodb.net/?retryWrites=true&w=majority
+mongoose.connect(
+  `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@cluster0.gqtpff3.mongodb.net/${process.env.MONGO_DB_DATABASE}?retryWrites=true&w=majority`,
+  ).then(() => {
+    console.log("Database connected!")
+  });
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use("/api", userRoutes);
+
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
