ae51cb978b1049ce2a1e95d9b5f86cbe42ab1338
Lucas <lucasbivarfonseca@gmail.com>
2022-07-07 15:17:26 -0300

    fixing try to sign out with expired token

--- src/routes/admin/auth.js
+++ src/routes/admin/auth.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { requireSignin } = require("../../common-middleware");
+// const { requireSignin } = require("../../common-middleware");
 const router = express.Router();
 const { signup, signin, signout} = require("../../controller/admin/auth");
 const {validateSignupRequest, validateSigninRequest, isRequestValidated} = require("../../validators/auth");
@@ -8,6 +8,6 @@ router.post("/admin/signup", validateSignupRequest, isRequestValidated, signup);
 
 router.post("/admin/signin", validateSigninRequest, isRequestValidated, signin);
 
-router.post("/admin/signout", requireSignin, signout);
+router.post("/admin/signout", signout);
 
 module.exports = router;
