bd73240dc1d08de41bf26edc8fa4ba9224a8bec3
Lucas <lucasbivarfonseca@gmail.com>
2022-07-07 00:04:14 -0300

    creating signout route

--- src/routes/admin/auth.js
+++ src/routes/admin/auth.js
@@ -1,15 +1,13 @@
 const express = require("express");
+const { requireSignin } = require("../../common-middleware");
 const router = express.Router();
-const { signup, signin } = require("../../controller/admin/auth");
+const { signup, signin, signout} = require("../../controller/admin/auth");
 const {validateSignupRequest, validateSigninRequest, isRequestValidated} = require("../../validators/auth");
 
 router.post("/admin/signup", validateSignupRequest, isRequestValidated, signup);
 
 router.post("/admin/signin", validateSigninRequest, isRequestValidated, signin);
 
-// router.post("/profile", requireSignin, (req, res) => {
-//   return res.status(200).json({user: req.user});
-// });
-
+router.post("/admin/signout", requireSignin, signout);
 
 module.exports = router;
